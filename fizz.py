6970fb729163d27d0a262e5222949026e30147f5
Phillip McCurdy <phillipmccurdy@Phillips-MacBook-Air.local>
2018-03-14 00:03:20 -0700

    initial

---
+++ fizz.py
@@ -0,0 +1,17 @@
+#!bin/env python
+
+def fizzbuzz(n):
+    num_list = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            num_list.append('FizzBuzz')
+        elif i % 3 == 0:
+            num_list.append('Fizz')
+        elif i % 5 == 0:
+            num_list.append('Buzz')
+        else:
+            num_list.append(str(i))
+
+    return num_list
+
+print fizzbuzz(15)
